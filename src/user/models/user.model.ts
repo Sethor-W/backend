c2d23887408dd8859543b0a6ecf4e171c45c2e95
Pedro Fernandez <pefernandez1408@gmail.com>
2024-03-05 10:56:26 -0400

    ADD: User model created

---
+++ src/user/models/user.model.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  rut: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  phone: string;
+}
---
+++ src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserService } from './user.service';
+import { User } from './models/user.model';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  providers: [UserService],
+})
+export class UserModule {}
